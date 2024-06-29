0d7ad05998b03e5c29d12dc3a6d31560101c3c53
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-06-29 10:29:02 +0300

    add thingies

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use rust_apt::*;
 use std::process::{Command, Stdio};
 use duct::cmd;
 use regex::Regex;
-use env::args;
+use std::fs;
 
 use clap::Parser;
 
@@ -21,7 +21,10 @@ struct Args {
     operation: String,
     #[arg(short, long)]
     /// The package to run the opreation on.
-    bin_pkg_name: String,
+    name: String,
+    #[arg(short, long)]
+    /// The package arch (amd64/i386/all)
+    arch: String,
     #[arg(short, long)]
     /// the file to output to.
     file_path: String
@@ -30,10 +33,21 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
+    match args.operation.as_str() {
+        "get_rdepends" => {
+            get_rdepends_source_name(args.name, args.arch, args.file_path)
+        }
+        "get_depends" => todo!(),
+        _ => {
+            eprintln!("Error: --operation is invalid!");
+            exit(2)
+        }
+    }
+
     println!("Hello {}!", args.operation);
 }
 
-fn get_rdepends_source_name() {
+fn get_rdepends_source_name(bin_pkg_name: String, bin_pkg_arch: String, output_file_path: String) {
     let mut rdepends_vec = Vec::new();
 
     let mut srcbins_vec = Vec::new();
@@ -41,7 +55,7 @@ fn get_rdepends_source_name() {
     // load apt cache
     let cache = new_cache!().unwrap();
 
-    let apt_cache_command = match cmd!("apt-cache", "showsrc", "libgtk-4-dev").env("LANG", "en_US.UTF-8").stderr_capture().stdout_capture().run() {
+    let apt_cache_command = match cmd!("apt-cache", "showsrc", bin_pkg_name).env("LANG", "en_US.UTF-8").stderr_capture().stdout_capture().run() {
         Ok(t) => {
             if String::from_utf8(t.stderr).unwrap().contains("Unable to locate package") {
                 eprintln!("Selected Package has no Source Package entry");
@@ -68,27 +82,59 @@ fn get_rdepends_source_name() {
 
     for src_bin in &srcbins_vec {
         // get record for requested package
-        match cache.get(src_bin) {
-            Some(pkg) => {
-                match pkg.rdepends().get(&DepType::Depends) {
-                    Some(rdep_iter) => {
-                        for dep in rdep_iter {
-                            match dep.first().target_package().candidate() {
-                                Some(t) => rdepends_vec.push(t.source_name().to_owned()),
-                                None => {}
+        match bin_pkg_arch.as_str() {
+            "all" => {
+                match cache.get(src_bin) {
+                    Some(pkg) => {
+                        match pkg.rdepends().get(&DepType::Depends) {
+                            Some(rdep_iter) => {
+                                for dep in rdep_iter {
+                                    match dep.first().target_package().candidate() {
+                                        Some(t) => rdepends_vec.push(t.source_name().to_owned()),
+                                        None => {}
+                                    }
+                                }
                             }
+                            _ => {}
                         }
                     }
                     _ => {}
-                }
+                };
             }
-            _ => {}
-        };
+            _ => {
+                match cache.get(&(src_bin.to_string() + ":" + &bin_pkg_arch)) {
+                    Some(pkg) => {
+                        match pkg.rdepends().get(&DepType::Depends) {
+                            Some(rdep_iter) => {
+                                for dep in rdep_iter {
+                                    match dep.first().target_package().candidate() {
+                                        Some(t) => rdepends_vec.push(t.source_name().to_owned()),
+                                        None => {}
+                                    }
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                    _ => {}
+                };
+            }
+        }
     }
     // Dedup to make get source names faster
     rdepends_vec.sort_unstable();
     rdepends_vec.dedup();
-    for i in rdepends_vec.iter() {
-        println!("{}", i)
+
+    let encoded_rdepends_vec = rdepends_vec
+        .iter()
+        .map(|pkg| format!("{pkg}"))
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    fs::remove_file(&output_file_path).expect("File already exists and can't be removed");
+    fs::write(output_file_path, encoded_rdepends_vec).expect("Could not write file");
+
+    for rdepend in rdepends_vec.iter() {
+        println!("{}", rdepend)
     }
 }
